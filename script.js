f220f4f49e0a262a98a39925730caff0ec5d263d
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2025-03-23 11:15:46 -0300

    Add js file that creates the grid (32 divs);

---
+++ script.js
@@ -0,0 +1,8 @@
+let gridContainer;
+
+let container = document.querySelector(".container");
+
+for(i=0; i<32; i++){
+    gridContainer = document.createElement("div");
+    container.appendChild(gridContainer);
+}
