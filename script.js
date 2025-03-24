e4394afce1521a9d63806f45981a39985bb0f073
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2025-03-24 11:09:55 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-let gridContainer;
-
+let rowContainer;
+let cell;
 let container = document.querySelector(".container");
 
-for(i=0; i<32; i++){
-    gridContainer = document.createElement("div");
-    container.appendChild(gridContainer);
+for(i=1; i<=16; i++){
+    rowContainer = document.createElement("div");
+    rowContainer.style.display = "flex";
+    rowContainer.style.justifyContent = "center";
+    container.appendChild(rowContainer);
+    for(j=1; j<=16; j++){
+        cell = document.createElement("div");
+        cell.style.backgroundColor = "green";
+        cell.style.height = "40px"
+        cell.style.width = "40px"
+        let cellNumber = 16*i+j;
+        cell.id = `cell${cellNumber}`;
+        cell.classList.add("cell");
+        rowContainer.appendChild(cell);
+    }
 }
+
+container.addEventListener("mouseover", function(event){
+    if(event.target.className === "cell"){
+        let cellHoveredId = "#" + event.target.id;
+        console.log(cellHoveredId);
+        let cellHovered = document.querySelector(cellHoveredId);
+        cellHovered.style.backgroundColor = "red";
+    }
+
+
+});
+container.addEventListener("mouseout", function(event){
+    if(event.target.className === "cell"){
+        let cellHoveredId = "#" + event.target.id;
+        console.log(cellHoveredId);
+        let cellHovered = document.querySelector(cellHoveredId);
+        cellHovered.style.backgroundColor = "green";
+    }
+});
