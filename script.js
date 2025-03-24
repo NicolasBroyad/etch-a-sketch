1580fcc850ae11da8b3178d06e43a74e67e843df
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2025-03-24 12:24:27 -0300

    Add button js functionality Make button call the createGrid function with the size entered on the input

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let rowContainer;
 let cell;
 let container = document.querySelector(".container");
 
-let rows = 16;
-let columns = 16;
-
+let rows = columns = 16;
 
 function createGrid(rows, columns){
     for(i=1; i<=rows; i++){
         rowContainer = document.createElement("div");
         rowContainer.style.display = "flex";
         rowContainer.style.justifyContent = "center";
+        rowContainer.style.flex = "1";
         container.appendChild(rowContainer);
         for(j=1; j<=columns; j++){
             cell = document.createElement("div");
             cell.style.backgroundColor = "green";
-            cell.style.height = "40px"
-            cell.style.width = "40px"
+            cell.style.flex = "1";
+            // cell.style.height = "40px"
+            // cell.style.width = "40px"
             let cellNumber = columns*i+j;
             cell.id = `cell${cellNumber}`;
             cell.classList.add("cell");
@@ -32,16 +32,33 @@ container.addEventListener("mouseover", function(event){
         let cellHoveredId = "#" + event.target.id;
         console.log(cellHoveredId);
         let cellHovered = document.querySelector(cellHoveredId);
-        cellHovered.style.backgroundColor = "red";
+        cellHovered.style.backgroundColor = "white";
     }
 
 
 });
-container.addEventListener("mouseout", function(event){
-    if(event.target.className === "cell"){
-        let cellHoveredId = "#" + event.target.id;
-        console.log(cellHoveredId);
-        let cellHovered = document.querySelector(cellHoveredId);
-        cellHovered.style.backgroundColor = "green";
+// container.addEventListener("mouseout", function(event){
+//     if(event.target.className === "cell"){
+//         let cellHoveredId = "#" + event.target.id;
+//         console.log(cellHoveredId);
+//         let cellHovered = document.querySelector(cellHoveredId);
+//         cellHovered.style.backgroundColor = "green";
+//     }
+// });
+
+let button = document.querySelector("button");
+button.addEventListener("click",function(){
+    rows = columns = Number(prompt("Type the number of squares per size for the new grid (max 100): "));
+    if(typeof(rows) !== "number" || isNaN(rows)){
+        alert("The entered value is not a number")
+    } else if(rows > 100){
+        alert("The entered number is higher than 100");
+    } else if(rows === 0){
+        alert("The entered number can not be 0 (cero)");
+    } else {
+        // alert("You entered " + rows);
+        container.innerHTML = "";
+        createGrid(rows, columns);
     }
-});
+
+})
