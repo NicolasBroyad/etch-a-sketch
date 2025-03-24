fa9a83ee55d89ea7089e2a894d847d356fd1c040
NicolasBroyad <113180571+NicolasBroyad@users.noreply.github.com>
2025-03-24 13:14:59 -0300

    Add restart button

--- script.js
+++ script.js
@@ -30,35 +30,31 @@ createGrid(rows,columns);
 container.addEventListener("mouseover", function(event){
     if(event.target.className === "cell"){
         let cellHoveredId = "#" + event.target.id;
-        console.log(cellHoveredId);
         let cellHovered = document.querySelector(cellHoveredId);
         cellHovered.style.backgroundColor = "white";
     }
 
 
 });
-// container.addEventListener("mouseout", function(event){
-//     if(event.target.className === "cell"){
-//         let cellHoveredId = "#" + event.target.id;
-//         console.log(cellHoveredId);
-//         let cellHovered = document.querySelector(cellHoveredId);
-//         cellHovered.style.backgroundColor = "green";
-//     }
-// });
 
-let button = document.querySelector("button");
-button.addEventListener("click",function(){
-    rows = columns = Number(prompt("Type the number of squares per size for the new grid (max 100): "));
-    if(typeof(rows) !== "number" || isNaN(rows)){
+let gridSizeButton = document.querySelector("#grid-size-button");
+gridSizeButton.addEventListener("click",function(){
+    let userInput = Number(prompt("Type the number of squares per size for the new grid (max 100): "));
+    if(typeof(userInput) !== "number" || isNaN(userInput)){
         alert("The entered value is not a number")
-    } else if(rows > 100){
+    } else if(userInput > 100){
         alert("The entered number is higher than 100");
-    } else if(rows === 0){
+    } else if(userInput === 0){
         alert("The entered number can not be 0 (cero)");
     } else {
-        // alert("You entered " + rows);
         container.innerHTML = "";
+        rows = columns = userInput;
         createGrid(rows, columns);
     }
+})
 
+let restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click",function(){
+    container.innerHTML = "";
+    createGrid(rows, columns);
 })
